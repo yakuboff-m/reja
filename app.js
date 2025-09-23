f2197f458de4dcb4f9bc3aba398d76a79c772a25
yakuboff-m <m.yakuboff.15@gmail.com>
2025-09-23 15:32:05 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -28,9 +28,16 @@ app.set("view engine", "ejs");
 
 // 4: Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success"});
-    // TODO: code with db here
+    console.log("user entered /create-item");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
 })
 
 app.get("/author", (req, res) => {
@@ -38,7 +45,16 @@ app.get("/author", (req, res) => {
 })
 
 app.get("/", function(req, res) {
-    res.render("reja");
+    console.log("user entered /");
+    db.collection("plans").find().toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            console.log(data);
+            res.render("reja", { items: data });
+        }
+    })
 });
 
 module.exports = app;
