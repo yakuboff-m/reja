5a707408398aebf2a7133b56579985ee271ac28d
yakuboff-m <m.yakuboff.15@gmail.com>
2025-09-25 15:21:16 +0900

    feat: FrontEnd JavaScriptni qurish

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('Web Serverni boshlash');
+const { log } = require("console");
 const express = require("express");
 const app = express();
 const fs = require("fs");
@@ -31,12 +32,8 @@ app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.end("successfully added");
-        }
+        // console.log(data.ops);
+        res.json(data.ops[0]);
     });
 })
 
